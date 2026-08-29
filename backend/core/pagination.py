946d4c98fe8df074af4a06485c9269a302f0904b
Satya Ranjan Das <satya.das@appman.tech>
2026-08-29 14:35:40 +0530

    Final Changes Resolved

--- backend/core/pagination.py
+++ backend/core/pagination.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 12
     page_size_query_param = 'page_size'
-    max_page_size = 100
+    max_page_size = 1000
 
     def get_paginated_response(self, data):
         return Response({
